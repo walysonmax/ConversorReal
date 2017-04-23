8b0ced2bd04b0b9706cb0d050fbe4fe06628f67e
walyson <walysonmaxswell@gmail.com>
2017-04-23 00:42:26 -0300

    criação projeto

---
+++ ConversorReal/ConversorReal/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConversorReal.ViewModels;
+
+namespace ConversorReal.Infrastructure
+{
+    public class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanceLocator()
+        {
+            Main = new MainViewModel();
+        }
+
+    }
+}
